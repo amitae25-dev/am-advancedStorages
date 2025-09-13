485cccf1824736719ac7b19f723ea8d471b56f32
Amitae_ <amitae25@gmail.com>
2025-09-13 13:23:22 +0200

    Update main.lua
    Park In function added

--- client/main.lua
+++ client/main.lua
@@ -1,6 +1,18 @@
 CreateThread(function()
     Wait(2000)
-    InitTarget()
+    InitTarget(Config.NPC, true, Config.Translate['npc_target'], function()             -- Warehouse NPC
+        OpenStorageMain()
+    end)
+
+    --[[if Config.Robbery.enabled then 
+        InitTarget(Config.RobberyNPC, false, Config.Translate['robbery_target'], function()             -- Robbery NPC      Work in progress...
+            OpenRobberyMain()
+        end)
+    end]]
+
+    if Config.DefaultSettings.parkIn.enabled then 
+        InitParkIn(Config.DefaultSettings.parkIn.enterPos)
+    end
 end)
 
 exports("am_storages:isPlayerInside", isPlayerInside())
